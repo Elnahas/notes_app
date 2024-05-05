664293669724164b2b3568d9f5383783d83d9d5e
Hazem <hazemelnahas2010@gmail.com>
2024-05-05 18:17:10 +0300

    - Finish note bottom sheet

---
+++ lib/core/widgets/app_button_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notes_app/core/theming/color.dart';
+
+class AppButtonText extends StatelessWidget {
+  const AppButtonText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+          fixedSize: MaterialStateProperty.all(Size(double.maxFinite, 50.h)),
+          backgroundColor: MaterialStatePropertyAll(Colors.cyan)),
+      onPressed: () {},
+      child: Text(
+        "Add",
+        style: TextStyle(color: Colors.black, fontSize: 20.sp ),
+      ),
+    );
+  }
+}
--- lib/features/notes/ui/widgets/add_note_bottom_sheet.dart
+++ lib/features/notes/ui/widgets/add_note_bottom_sheet.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/core/helpers/spacing.dart';
 import 'package:notes_app/core/widgets/app_text_field.dart';
 
+import '../../../../core/widgets/app_button_text.dart';
+
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
@@ -16,6 +18,9 @@ class AddNoteButtonSheet extends StatelessWidget {
             const AppTextField(hint: "Title",),
             verticalSpace(16),
             const AppTextField(hint: "Content", maxLines: 5,),
+            verticalSpace(100),
+            const AppButtonText(),
+            verticalSpace(16),
             ],
         ),
       ),
